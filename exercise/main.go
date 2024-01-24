697a5b7ae687b0456f3a05e5a8172f2ba8f601f8
dan <2022156892@ub.edu.bz>
2024-01-24 13:17:56 -0600

    exercise1

---
+++ exercise/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Sstring string
+var Bbyte byte
+var Bbool bool
+
+func main() {
+	size := 44.44
+	fmt.Printf("%v\n", size)
+	fmt.Println(Sstring, Bbyte, Bbool)
+}
