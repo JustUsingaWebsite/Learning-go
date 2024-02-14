685ae816e346d541f9f51293f5257ce7d2f60f66
dan <2022156892@ub.edu.bz>
2024-02-14 13:13:33 -0600

    comments

--- daniel_rosales/partA/main.go
+++ daniel_rosales/partA/main.go
@@ -8,25 +8,31 @@ import (
 
 type circle struct {
 	radius float64
-}
+} // made the easy struct
 
 func (c *circle) area() (float64, error) {
 	if c.radius < 0 {
 		return 0, errors.New("radius cannot be negative")
 	}
 	return math.Pi * c.radius * c.radius, nil
-}
+} // simple check if it's negative if so return err
 
 func (c *circle) circumference() (float64, error) {
 	if c.radius < 0 {
 		return 0, errors.New("radius cannot be negative")
 	}
 	return 2 * math.Pi * c.radius, nil
-}
+} // another simple check for negative numbers if true return err
 
 func main() {
 
-	c := circle{radius: 5.0}
+	//make a variable and call the area func
+	//make sure to err check
+
+	//make another variable and call circumference func
+	//make sure to err check and handle it properly
+
+	c := circle{radius: 5}
 
 	area, err := c.area()
 	if err != nil {
