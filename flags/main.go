4a832945ad65bed391a49f7232849a250a4f826f
dan <2022156892@ub.edu.bz>
2024-02-19 13:06:55 -0600

    caps implementation

--- flags/main.go
+++ flags/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
 
 func main() {
 	msg := flag.String("text", "Hello there!", "messsage to display")
+
+	caps := flag.Bool("c", false, "should text be uppercase")
 	flag.Parse()
+
+	if *caps {
+		*msg = strings.ToUpper(*msg)
+	}
+
 	fmt.Println(*msg)
 }
