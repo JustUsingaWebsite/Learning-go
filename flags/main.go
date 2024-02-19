6ffcb85845a3010a2ce7bdb0cd5b5f83452ed827
dan <2022156892@ub.edu.bz>
2024-02-19 13:15:45 -0600

    added num of lines

--- flags/main.go
+++ flags/main.go
@@ -10,11 +10,15 @@ func main() {
 	msg := flag.String("text", "Hello there!", "messsage to display")
 
 	caps := flag.Bool("c", false, "should text be uppercase")
+	nums := flag.Int("n", 1, "number of times is displayed")
 	flag.Parse()
 
 	if *caps {
 		*msg = strings.ToUpper(*msg)
 	}
 
-	fmt.Println(*msg)
+	for i := 0; i < *nums; i++ {
+		fmt.Println(*msg)
+	}
+
 }
