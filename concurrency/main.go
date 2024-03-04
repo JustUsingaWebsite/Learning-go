a0b2b1c888b5b84b4338ab85d59874f9d30b92b0
dan <2022156892@ub.edu.bz>
2024-03-04 13:10:17 -0600

    handling blocking with read/writing and sending/reciving data on channels

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
-
-func count(thing string, c chan string) {
-	for i := 1; i <= 5; i++ {
-		c <- thing
-		time.Sleep(time.Millisecond * 500)
-	}
-
-	close(c)
-}
 
 func main() {
 	c := make(chan string)
-	go count("sheep", c)
+	c <- "hello"
 
-	for msg := range c {
-		fmt.Println(msg)
-	}
+	msg := <-c
+	fmt.Println(msg)
 }
