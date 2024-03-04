2ea950d60ee911bf4e7a8c84ed8798d7dcccfba7
dan <2022156892@ub.edu.bz>
2024-03-04 13:15:57 -0600

    using two channels implementation

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	c := make(chan string, 2)
-	c <- "hello"
-	c <- "world"
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-	msg := <-c
-	fmt.Println(msg)
+	go func() {
+		for {
+			c1 <- "every 500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
 
-	msg = <-c
-	fmt.Println(msg)
+	go func() {
+		for {
+			c2 <- "every 2 seconds"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	for {
+		fmt.Println(<-c1)
+		fmt.Println(<-c2)
+	}
 }
