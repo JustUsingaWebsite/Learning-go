8cd69369031ff46767f240e9ad0594075886e43a
dan <2022156892@ub.edu.bz>
2024-04-22 13:14:23 -0600

    web-app

--- web-app/main.go
+++ web-app/main.go
@@ -16,7 +16,7 @@ func main() {
 	Welcome := Welcome{"Sale Begins now'", time.Now().Format(time.Stamp)}
 	template := template.Must(template.ParseFiles("template/template.html"))
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("sataic"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		if sale := r.FormValue("sale"); sale != "" {
@@ -24,7 +24,9 @@ func main() {
 		}
 
 		if err := template.ExecuteTemplate(w, "template.html", Welcome); err != nil {
+			// http.Error automatically sets the appropriate status code
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	})
 
